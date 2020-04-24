e23ce413084618ea1321054edd64cf7eae6e1561
sshresth <shreyashresth05@gmail.com>
2020-04-24 15:37:03 +0200

    Initial Commit

---
+++ FaithInDailyLife/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FaithInDailyLife
+//
+//  Created by Shreya Shresth on 24.04.20.
+//  Copyright © 2020 Shreya Shresth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
